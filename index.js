5cd54abc07dcff98000f8b811d1e419a0c2304de
maruf <dev.maruf112@gmail.com>
2022-10-26 04:51:30 +0600

    server setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+// // GET method route
+// app.get("/", (req, res) => {
+//   res.send("GET request to the homepage");
+// });
+app.listen(port, () => {
+  console.log("Dragon News Server running on port", port);
+});
