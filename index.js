b6d29ec1a7787811a091c9145de7482c549e82ef
maruf <dev.maruf112@gmail.com>
2022-10-26 06:20:55 +0600

    get data successfully

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
-// respond with "hello world" when a GET request is made to the homepage
+
+const courses = require("./data/courses.json");
+const coursesDetails = require("./data/coursesDetails.json");
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-// // GET method route
-// app.get("/", (req, res) => {
-//   res.send("GET request to the homepage");
-// });
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+app.get("/courses-details", (req, res) => {
+  res.send(coursesDetails);
+});
 app.listen(port, () => {
   console.log("Dragon News Server running on port", port);
 });
